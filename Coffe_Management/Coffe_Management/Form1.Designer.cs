da7fa548db67606b107143fc163f966ef3d34b92
Thanh <nlbthanh@gmail.com>
2023-10-25 15:29:14 +0700

    Add Project Buoi8

---
+++ Coffe_Management/Coffe_Management/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Coffe_Management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tb_Name_input = new System.Windows.Forms.TextBox();
+            this.tb_Details_input = new System.Windows.Forms.TextBox();
+            this.tb_Price_input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.tb_Find = new System.Windows.Forms.TextBox();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 176);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tb_Name_input
+            // 
+            this.tb_Name_input.Location = new System.Drawing.Point(110, 218);
+            this.tb_Name_input.Name = "tb_Name_input";
+            this.tb_Name_input.Size = new System.Drawing.Size(100, 20);
+            this.tb_Name_input.TabIndex = 1;
+            // 
+            // tb_Details_input
+            // 
+            this.tb_Details_input.Location = new System.Drawing.Point(110, 265);
+            this.tb_Details_input.Name = "tb_Details_input";
+            this.tb_Details_input.Size = new System.Drawing.Size(100, 20);
+            this.tb_Details_input.TabIndex = 1;
+            // 
+            // tb_Price_input
+            // 
+            this.tb_Price_input.Location = new System.Drawing.Point(110, 316);
+            this.tb_Price_input.Name = "tb_Price_input";
+            this.tb_Price_input.Size = new System.Drawing.Size(100, 20);
+            this.tb_Price_input.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Details";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price";
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(269, 218);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(111, 44);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(269, 281);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(111, 44);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // tb_Find
+            // 
+            this.tb_Find.Location = new System.Drawing.Point(406, 294);
+            this.tb_Find.Name = "tb_Find";
+            this.tb_Find.Size = new System.Drawing.Size(135, 20);
+            this.tb_Find.TabIndex = 1;
+            this.tb_Find.Text = "Input Name To Delete";
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.Location = new System.Drawing.Point(269, 346);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(111, 44);
+            this.btn_Refresh.TabIndex = 3;
+            this.btn_Refresh.Text = "Refresh";
+            this.btn_Refresh.UseVisualStyleBackColor = true;
+            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Refresh);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Price_input);
+            this.Controls.Add(this.tb_Details_input);
+            this.Controls.Add(this.tb_Find);
+            this.Controls.Add(this.tb_Name_input);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tb_Name_input;
+        private System.Windows.Forms.TextBox tb_Details_input;
+        private System.Windows.Forms.TextBox tb_Price_input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.TextBox tb_Find;
+        private System.Windows.Forms.Button btn_Refresh;
+    }
+}
+
---
+++ Coffe_Management/Coffe_Management/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Coffe_Management
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Data Source=PIOTISK\SQLEXPRESS_19;Initial Catalog=QL_Coffee;Integrated Security=True";
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Load_Data_Form_Sql()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Name, Details, Price FROM Menu";
+                SqlDataAdapter dataadapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                connection.Open();
+                dataadapter.Fill(dataTable);
+                connection.Close();
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                cmd = new SqlCommand("INSERT INTO Menu(Name, Details, Price) VALUES('" + tb_Name_input.Text + "','" + tb_Details_input.Text + "','" + Convert.ToInt32(tb_Price_input.Text.ToString()) + "')", connection);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show(" Data has Saved in Database !");
+                connection.Close();
+            }
+            tb_Details_input.Clear();
+            tb_Name_input.Clear();
+            tb_Price_input.Clear();
+        }
+        
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                cmd = new SqlCommand("DELETE FROM Menu WHERE Name = @Name", connection);
+                cmd.Parameters.AddWithValue("@Name", tb_Find.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Delete Successfully !");
+                connection.Close();
+            }
+            tb_Find.Clear();
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            Load_Data_Form_Sql();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Load_Data_Form_Sql();
+        }
+    }
+}
